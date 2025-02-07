35e9bdee7f1d79656077ea4ce3b67bbdb1a2b7b4
freddie.c.kuo <freddie.c.kuo@gmail.com>
2025-02-07 11:38:39 +0800

    Add Search Bar to Map

--- SingNature/SingNature/obj/Debug/net9.0/SingNature.AssemblyInfo.cs
+++ SingNature/SingNature/obj/Debug/net9.0/SingNature.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SingNature")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cfddc9f31f8d5e7a8460a2f119c47b64af66b02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74e2b62f379d6de110208a221a86c457a1cb65bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SingNature")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SingNature")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

e6651478269c1e0c51c7dfd1af98940ab9482ed5
NkkllS <nkklls01@gmail.com>
2025-02-09 18:02:33 +0800

    version

--- SingNature/SingNature/obj/Debug/net9.0/SingNature.AssemblyInfo.cs
+++ SingNature/SingNature/obj/Debug/net9.0/SingNature.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SingNature")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0538f7b5f179ce4ea424a68b178334a18e4ea052")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13f07fd488142fc6c2cef1e8c23f2f46fbe9a30")]
 [assembly: System.Reflection.AssemblyProductAttribute("SingNature")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SingNature")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
